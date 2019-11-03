32e74657b57ac271b2180bdc77579b94514d8a04
Jacob Yoon <jacobyoon926@gmail.com>
2019-11-03 16:07:29 -0500

    Design Change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     //Parallax effect
     if ($(window).width() > 600) {
-		var $bg = $('#landerimg');
+		var $bg = $('.bg');
 		var $win = $(window);
 		$win.on('scroll', function() {
-			var top1 = $win.scrollTop()/4;
-			$bg.css('top', '' + top1 + 'px');
+			var top1 = -$win.scrollTop()/2;
+			//$bg.css('top', '' + top1 + 'px');
+			$bg.css('transform', 'scale(' + (1.4 + top1 / 1000) + ')');
+			$bg.css('filter', 'saturate(0) opacity(' + (1 + top1 / 300) + ')');
 		});
 	}
 });
